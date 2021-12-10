4129954311671a740c856c57d90cec5d0c51fb76
Sllambias <llambias@live.com>
2021-12-10 16:08:39 +0100

    n

--- heatmap_creation.py
+++ heatmap_creation.py
@@ -129,7 +129,7 @@ class heatmapper:
             ### ADD 0-255 (or 0-1?) SCALING FOR EACH HEATMAP... or for entire set of heatmaps??
             heatmaps[idx] = heatmap[np.newaxis, strt:self.dims[1]+strt,strt:self.dims[0]+strt]
             torch.cuda.empty_cache()
-        return heatmaps
+        return heatmaps.cpu()
 
 
 #%%
