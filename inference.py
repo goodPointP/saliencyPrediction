f421b70d7e81222c4c628ebdacc1da74b42c60aa
Sllambias <llambias@live.com>
2021-12-15 21:03:14 +0100

    .

--- inference.py
+++ inference.py
@@ -11,16 +11,18 @@ import sys
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 parser = argparse.ArgumentParser(description='model inference')
 parser.add_argument('-m', default=None, required=True, dest='model')
-parser.add_argument('-i', default=None, required=True, dest='input', nargs="+")
+parser.add_argument('-i_tuple', default=None, dest='input',type=int, nargs="+")
+parser.add_argument('-i_data', default=None,  dest='input')
 parser.add_argument('-o', default=None, required=True, dest='outfile')
 parser.add_argument('-s', default=False, required=False, dest='save')
 parser.add_argument('-so', default=None, required='-s' in sys.argv, dest='dataset_outfile')
 # parser.add_argument('-t', default=None, dest='ground_truth', type=bool)
 
-args = parser.parse_args(['-m', "models/newnet_model", "-o", "models/inferencetest" ,"-i", "0 10"])
-print(type(args.input))
-#%%
+args = parser.parse_args(['-m', "models/newnet_model", "-o", "models/inferencetest"])
 
+if not args.input:
+    sys.exit("input not found. Use '-i tuple' or '-i data' with two ints or a dataloader, respectively")
+    
 accuracy = 0
 heatmaps = []
 
@@ -31,7 +33,7 @@ gazenet.to(device)
 gazenet.eval()
 
 print("constructing dataset")
-if type(args.input) == int:
+if len(args.input) > 1:
     args.input = tuple(args.input)
     dataset = inference_pipe(args.input, batch_size = 1, workers = 10)
     if args.save:
